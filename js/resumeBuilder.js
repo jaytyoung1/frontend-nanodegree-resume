65c917a16819b932cc9d2530c9b826b5f36a100a
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2017-09-27 13:28:56 -0400

    Build the resume header.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,7 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-
+/*
 var email = "jaytyoung1@udacity.com";
 var newEmail = email.replace("udacity", "gmail");
 
@@ -13,3 +13,11 @@ var awesomeThoughts = "I am Jay and I am AWESOME!";
 var funThoughts = awesomeThoughts.replace("AWESOME!", "FUN");
 
 $("#main").append(funThoughts);
+*/
+name = "Jay Young";
+role = "Python Developer";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
