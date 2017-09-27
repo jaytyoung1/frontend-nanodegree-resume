122ff7e9b8a0879d852d88ee5df1a356757f31ae
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2017-09-27 13:15:15 -0400

    Demonstrate string.replace() method.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,14 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- //$("#main").append("Jay Young")
+//$("#main").append("Jay Young")
 
- var awesomeThoughts = "I am Jay and I am AWESOME!"
+//("var awesomeThoughts = "I am Jay and I am AWESOME!"
 
-console.log(awesomeThoughts);
+//console.log(awesomeThoughts);
+
+var email = "jaytyoung1@udacity.com";
+var newEmail = email.replace("udacity", "gmail");
+
+console.log(email);
+console.log(newEmail);
