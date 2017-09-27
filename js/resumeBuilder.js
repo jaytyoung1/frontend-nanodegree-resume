e496cc048cb1f55354e057c705edbc6f0292c4db
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2017-09-27 13:11:37 -0400

    Create variable and print it to the console.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append("Jay Young")
+ //$("#main").append("Jay Young")
+
+ var awesomeThoughts = "I am Jay and I am AWESOME!"
+
+console.log(awesomeThoughts);
