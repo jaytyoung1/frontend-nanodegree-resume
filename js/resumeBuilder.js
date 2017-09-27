0b4e3dfea7e8b46b65ca270ebc711202a4195cd7
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2017-09-27 13:20:12 -0400

    Append funThoughts to the web page.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,14 +1,15 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-//$("#main").append("Jay Young")
-
-//("var awesomeThoughts = "I am Jay and I am AWESOME!"
-
-//console.log(awesomeThoughts);
 
 var email = "jaytyoung1@udacity.com";
 var newEmail = email.replace("udacity", "gmail");
 
 console.log(email);
 console.log(newEmail);
+
+var awesomeThoughts = "I am Jay and I am AWESOME!";
+
+var funThoughts = awesomeThoughts.replace("AWESOME!", "FUN");
+
+$("#main").append(funThoughts);
