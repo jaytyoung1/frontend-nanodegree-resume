82e590b0c4f26d9470564a50bc53186491f894a3
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2017-09-28 08:13:35 -0400

    Fill out work and projects objects.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,20 +1,3 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-/*
-var email = "jaytyoung1@udacity.com";
-var newEmail = email.replace("udacity", "gmail");
-
-console.log(email);
-console.log(newEmail);
-
-var awesomeThoughts = "I am Jay and I am AWESOME!";
-
-var funThoughts = awesomeThoughts.replace("AWESOME!", "FUN");
-
-$("#main").append(funThoughts);
-*/
-
 var bio = {
 	"name": "Jay Young",
 	"role": "Python Developer",
@@ -28,6 +11,65 @@ var bio = {
 	"welcomeMessage": "",
 	"skills": ["Python programming", "Web scraping", "PostgreSQL", "HTML", "CSS", "JavaScript"],
 	"bioPic": "images/headshot.png"
+}
+
+var education = {
+	"schools": [
+		{
+			"name": "East Stroudsburg University",
+			"location": "East Stroudsburg, PA",
+			"degree": "BS",
+			"majors": ["Computer Science"],
+			"dates": 2017,
+			"url": "http://www.esu.edu/"
+		}
+	],
+	"onlineCourses": [
+		{
+			"title": "JavaScript Syntax",
+			"school": "Udacity",
+			"dates": 2017,
+			"url": "http://www.udacity.com/course/ud804"
+		}
+	]
+}
+
+var work = {
+	"jobs": [
+		{
+			"employer": "RotoGrinders.com",
+			"title": "Python Developer",
+			"dates": 2017,
+			"description": "Worked as a python programmer, developed auditing, data scraping, and Twitter automation programs."
+		},
+		{
+			"employer": "FD Software Enterprises",
+			"title": "Junior Programmer",
+			"dates": 2016,
+			"description": "Contributed to the Android application development tasks such as UI development, testing, and coding."
+		}
+	]
+}
+
+var projects = {
+	"projects": [
+		{
+			"title": "DFS-Twitter-Alerts",
+			"dates": 2017,
+			"description": "This project uses the Python library, Tweepy, to capture a live stream of tweets from members of one of RotoGrinders Twitter lists and automatically send the tweet and metadata as a message in a private Slack channel",
+			"images": [
+				"http://diysolarpanelsv.com/images/twitter-logo-clipart-free-20.jpg"
+			]
+		},
+		{
+			"title": "FanDuel Contests",
+			"dates": 2017,
+			"description": "This project uses the FanDuel API to collect metadata for finished FanDuel contests specified by the user. It retrieves the data, organizes it into a JSON format, then uploads the .json file to an Amazon Web Services S3 bucket.",
+			"images": [
+				"http://www.underconsideration.com/brandnew/archives/fanduel_monogram_detail.png"
+			]
+		}
+	]
 }
 
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
